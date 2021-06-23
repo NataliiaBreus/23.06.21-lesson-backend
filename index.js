17cf72ce40a4bcd7f82261283049dd9f0eb6d666
Nataliia Breus <breusnataly@gmail.com>
2021-06-23 10:26:44 +0300

    added folder views

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require ('http');
+const fs = require('fs').promises
 
-const requestListener=async (req, res) => {
-  console.log(req);
+const routerGET = {
+  '/': async (req, res) => {
+    const data = await fs.readFile('./views/index.html', 'utf8')
+    res.end(data)
+  },
+  '/contacts.html': async (req, res) => {
+    const data = await fs.readFile('./views/contacts.html', 'utf8')
+    res.end(data)
+  },
+  '/about.html': async (req, res) => {
+    const data = await fs.readFile('./views/about.html', 'utf-8')
+  },
+
 }
 
-const server = http.createServer()
+const requestListener=async (req, res) => {
+  if (routerGET[req.url]){
+    return routerGET [req.url](req, res)
+  }
+  const data = await fs.readFile('./views/404.html', 'utf-8')
+
+  res.end(data)
+}
+
+const server = http.createServer(requestListener)
  server.listen(3000)
