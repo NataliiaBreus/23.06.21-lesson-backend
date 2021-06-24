9bf814152b0a9473f5f3723a6743b618b7623131
Nataliia Breus <breusnataly@gmail.com>
2021-06-24 12:10:41 +0300

    changes

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express')
+
+const { createUser } 
 const app = express()
 
-const PORT = 3000
+const jsonParser = express.json()
 
-app.get('/', (req, res) =>{
+const validationSchema = yup.object({
+  name: yup.string
+  .trim()
+  .min(3)
+  .required(),
+  email: yup
+  .string()
+  .trim()
+  .email()
+  .required(),
+  password: yup
+  .string()
+  .trim()
+  .required(),
+  gender: yup
+  .string()
+  .trim()
+  .required(),
+  isSubscribed: yup.boolean(),
+});
+
+validationSchema.validate()
+
+const PORT = 3000;
+
+app.get('/', (req, res, next) =>{
   res.send ('Hello world!')
-})
+});
 app.get('*', (req, res) =>{
   res.send('ERROR 404')
-})
+});
+app.post('/', jsonParser, async(req, res) =>{
+ req.body = await validationSchema.validate(req, body);
+});
 
 app.listen (PORT, () => {
   console.log('Server is running on ${PORT} port')
